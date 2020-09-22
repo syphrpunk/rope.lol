cb0c535b909ec6e6af9d710a5a5fa5652ddb7b5b
Lai Weng Han <jlwhoo7@gmail.com>
2020-09-23 04:03:00 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./other.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className="flex flex-col w-screen h-screen justify-between">
+      <div>
+        <header className="visible sm:invisible sm:hidden">
+          <div className="flex justify-end">
+            <img
+              className="object-contain"
+              src="/images/NFTz-coming-soon.png"
+              alt="soon"
+            />
+          </div>
+          <img
+            className="object-contain pl-4 -mt-16"
+            src="/images/ROPE.png"
+            alt="rope"
+          />
+        </header>
+        <header className="hidden invisible sm:flex sm:visible w-full justify-between">
+          <img
+            className="object-contain pl-4"
+            src="/images/ROPE.png"
+            alt="rope"
+          />
+          <img
+            className="object-contain"
+            src="/images/NFTz-coming-soon.png"
+            alt="soon"
+          />
+        </header>
+      </div>
+      <div className="flex flex-col lg:flex-row lg:justify-between lg:px-8">
+        <div className="flex">
+          <img
+            className="object-contain w-16"
+            src="/images/aropearrow.png"
+            alt="a rope arrow"
+          />
+          <img
+            className="object-contain"
+            style={{ minWidth: "340px" }}
+            src="/images/Noose-picture.png"
+            alt="noose"
+          />
+        </div>
+        <img
+          className="object-contain lg:w-64"
+          src="/images/DontBuyIt.png"
+          alt="dont buy it"
+        />
+        <div className="flex">
+          <img
+            className="object-contain"
+            style={{ minWidth: "340px" }}
+            src="/images/wojakpicture.png"
+            alt="wojak"
+          />
+          <img
+            className="object-contain w-16"
+            src="/images/you-arrow.png"
+            alt="you arrow"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col w-screen items-center lg:flex-row lg:justify-around pb-8">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="zoom py-2 lg:py-0"
+          href="https://twitter.com/dontbuyrope"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img
+            className="object-contain"
+            src="/images/Twitter.png"
+            alt="you arrow"
+          />
         </a>
-      </header>
+        <a
+          className="zoom py-2 lg:py-0"
+          href="https://app.uniswap.org/#/swap?inputCurrency=0x9d47894f8becb68b9cf3428d256311affe8b068b&outputCurrency=ETH"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="object-contain"
+            src="/images/Uniswap.png"
+            alt="uniswap"
+          />
+        </a>
+        <a
+          className="zoom py-2 lg:py-0"
+          href="https://t.me/dontbuyrope"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="object-contain"
+            src="/images/Telegram.png"
+            alt="you arrow"
+          />
+        </a>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
