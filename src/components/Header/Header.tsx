93b972e5af8cb5b8e94b11c575f32ab53d20349d
Lai Weng Han <jlwhoo7@gmail.com>
2020-09-24 23:40:24 +0800

    fix styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import AccountButton from "../TopBar/components/AccountButton";
+import React from 'react';
+import AccountButton from '../TopBar/components/AccountButton';
 
 export const Header = () => {
   return (
     <div>
-      <header className="visible sm:invisible sm:hidden">
-        <div className="flex justify-end">
+      <header className='visible sm:invisible sm:hidden'>
+        <div className='flex justify-end'>
           <img
-            className="object-contain"
-            src="/images/NFTz-coming-soon.png"
-            alt="soon"
+            className='object-contain'
+            src='/images/NFTz-coming-soon.png'
+            alt='soon'
           />
         </div>
         <img
-          className="object-contain pl-4 -mt-16"
-          src="/images/ROPE.png"
-          alt="rope"
+          className='object-contain pl-4 -mt-16'
+          src='/images/ROPE.png'
+          alt='rope'
         />
       </header>
-      <header className="hidden invisible sm:flex sm:visible w-full justify-between">
+      <header className='hidden invisible sm:flex sm:visible w-full justify-between'>
         <img
-          className="object-contain pl-4"
-          src="/images/ROPE.png"
-          alt="rope"
+          className='object-contain pl-4'
+          src='/images/ROPE.png'
+          alt='rope'
         />
-        <div className="mt-4">
+        <div className='mt-4'>
           <AccountButton />
         </div>
         <img
-          className="object-contain"
-          src="/images/NFTz-coming-soon.png"
-          alt="soon"
+          className='object-contain'
+          src='/images/NFTz-coming-soon.png'
+          alt='soon'
         />
       </header>
     </div>
--- src/pages/DontBuyPage.tsx
+++ src/pages/DontBuyPage.tsx
@@ -1,47 +1,47 @@
-import React from "react";
-import { Header } from "../components/Header/Header";
+import React from 'react';
+import { Header } from '../components/Header/Header';
 
 export const DontBuyPage: React.FC = () => {
   return (
-    <div className="flex flex-col w-screen h-screen justify-between">
+    <div className='flex flex-col w-screen h-screen justify-between'>
       <Header />
-      <div className="flex justify-center">
-        <img src="/images/DontBuyIt.png" alt="Dont Buy It!" />
+      <div className='flex justify-center'>
+        <img src='/images/DontBuyIt.png' alt='Dont Buy It!' />
       </div>
-      <div className="flex flex-col items-center px-4 md:px-0">
-        <div className="text-2xl md:text-4xl cursive pt-8">
+      <div className='flex flex-col items-center px-4 md:px-0'>
+        <div className='text-2xl md:text-4xl cursive pt-8'>
           Don't just take our word for it, these people also said don't buy rope
         </div>
-        <div className="py-8">
+        <div className='py-8'>
           <iframe
-            title="$ROPE - Gilbert Gottfried has a message for you Degens"
-            width="560"
-            height="315"
-            src="https://www.youtube-nocookie.com/embed/si8xvLSz9jU"
+            title='$ROPE - Gilbert Gottfried has a message for you Degens'
+            width='560'
+            height='315'
+            src='https://www.youtube-nocookie.com/embed/si8xvLSz9jU'
             frameBorder={0}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
             allowFullScreen
           ></iframe>
         </div>
-        <div className="py-4">
+        <div className='py-4'>
           <iframe
-            title="$ROPE - Zydrunas Savickas"
-            width="560"
-            height="315"
-            src="https://www.youtube-nocookie.com/embed/qSZRA9NMsLU"
+            title='$ROPE - Zydrunas Savickas'
+            width='560'
+            height='315'
+            src='https://www.youtube-nocookie.com/embed/qSZRA9NMsLU'
             frameBorder={0}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
             allowFullScreen
           ></iframe>
         </div>
-        <div className="py-4">
+        <div className='py-4'>
           <iframe
-            title="$ROPE - A message from the big guy himself for all the CHADS out there."
-            width="560"
-            height="315"
-            src="https://www.youtube-nocookie.com/embed/_Cw9Sqw5K98"
+            title='$ROPE - A message from the big guy himself for all the CHADS out there.'
+            width='560'
+            height='315'
+            src='https://www.youtube-nocookie.com/embed/_Cw9Sqw5K98'
             frameBorder={0}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
             allowFullScreen
           ></iframe>
         </div>
